0211373fced3086850f34f92b2f9af05b6c6d3de
Vladimir Kotal <vlada@devnull.cz>
2019-03-11 14:06:02 +0100

    add example

---
+++ src/array-designated-initializers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+char array[80] = { [0] = 'A', [2] = 'f', [4] = 'o', [6] = 'o' };
+
+int
+main(void)
+{
+	int i = 0;
+
+	while (i < sizeof (array) / sizeof (array[0])) {
+		if (array[i] != 0)
+			printf("%c", array[i]);
+		else
+			printf("_");
+
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
