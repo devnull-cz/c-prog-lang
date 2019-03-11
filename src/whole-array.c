f6fba00fb00dabf06321981f973f8becefa676d4
Vladimir Kotal <vlada@devnull.cz>
2019-03-12 00:31:12 +0100

    avoid for

--- src/whole-array.c
+++ src/whole-array.c
@@ -7,10 +7,12 @@ int array[] = {23, 34, 12, 17, 204, 99, 16};
 int
 main(void)
 {
-	int d;
+	int d = -1;
 
-	for (d = -1; d <= (TOTAL_ELEMENTS - 2); d++)
-	    printf("%d\n", array[d+1]);
+	while (d <= (TOTAL_ELEMENTS - 2)) {
+		printf("%d\n", array[d + 1]);
+		d++;
+	}
 
 	return (0);
 }
