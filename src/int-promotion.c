27d1e0a07fce51a57e55c115e37a65bf2a7341a7
Vladimir Kotal <vlada@devnull.cz>
2019-03-12 00:36:44 +0100

    Update int-promotion.c

--- src/int-promotion.c
+++ src/int-promotion.c
@@ -9,8 +9,9 @@ main(void)
 	int c = a == b; // true, or false ?
 
 	/*
-	 * Note that 'a' and 'b' are first promoted to ints (integer promotion),
-	 * then %x converts the arguments to UNSIGNED ints before printing
+	 * Note that 'a' and 'b' are first promoted to ints (integer promotion)
+	 * when they are being compared.
+	 * Also, %x converts the arguments to UNSIGNED ints before printing
 	 * those.
 	 */
 	printf("A: %08x, B: %08x\n", a, b);
