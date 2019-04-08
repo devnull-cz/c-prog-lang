2268aa67b20e15adad229cf1af6bc5a80a0417ec
Vladimir Kotal <vlada@devnull.cz>
2019-04-09 00:40:16 +0200

    add task

---
+++ src/ptr-to-array-of-ptrs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char *argv[])
+{
+	int i = 42, j = 7;
+	int *a[] = { &i, &j };
+	int b[] = { i, j };
+
+	if (argc != 2)
+		return(1);
+
+	int idx = atoi(argv[1]);
+
+	printf("%p %p\n", &i, &j);
+	printf("%p\n", a);
+	printf("%p\n", b);
+
+	int *p = a[idx];
+	int *q = &b[idx];
+
+	printf("%p %p\n", p, q);
+	printf("%d %d\n", *p, *q);
+
+	return (0);
+}
