5b57b762765f5ee39609a9abb068b76b6ff5d411
Vladimir Kotal <vlada@devnull.cz>
2019-04-09 08:56:35 +0200

    use uint

---
+++ src/fopen-leak.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+#include <limits.h>
+
+int
+main(int argc, char *argv[])
+{
+	if (argc != 2)
+		errx(1, "usage: %s <file>", argv[0]);
+
+	for (size_t i = 1; i < UINT_MAX; i++) {
+		FILE *f = fopen(argv[1], "r");
+		if (f == NULL)
+			err(1, "fopen [%zu]", i);
+	}
+
+	return (0);
+}
