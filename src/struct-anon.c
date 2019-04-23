9292c865f2ab5aac747c407c076e3ed4ff30a32b
Vladimir Kotal <vlada@devnull.cz>
2019-04-23 21:54:45 +0200

    add anon struct example

---
+++ src/struct-anon.c
@@ -0,0 +1,19 @@
+// compile with: -Wall -pedantic -std=c99
+
+#include <stdio.h>
+
+int
+main(void)
+{
+	struct {
+		struct {
+			int a;
+			char b;
+		};
+	} foo;
+
+	foo.a = 42;
+	printf("%d\n", foo.a);
+
+	return (0);
+}
