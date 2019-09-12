acc30c82b7c628d9e0c0f1e1464ea1e21009a02e
Vladimir Kotal <vlada@devnull.cz>
2019-09-12 23:48:59 +0200

    add variable check

--- dev/pages.sh
+++ dev/pages.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -n $GH_PAGES_TOKEN ]]; then
+	echo "GH_PAGES_TOKEN environment variable is missing"
+	exit 1
+fi
+
 git config --global user.email "vlada@devnull.cz"
 git config --global user.name "vladak"
 
