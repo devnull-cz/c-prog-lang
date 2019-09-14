4ed8488a65cb90b7576133b2f4146c00b897bf9f
Jan Pechanec <jp@devnull.cz>
2019-09-14 23:47:10 +0200

    Fixed comment formatting.

--- src/linked-list.c
+++ src/linked-list.c
@@ -8,41 +8,41 @@
  * Commands are: create a list, insert an item, remove an item, and print the
  * list.  It is mandatory to create a list before doing anything else.
  *
- *	create a list:		C
- *	insert an item:		I<nnn>
- *	remove an item:		R
- *	print a list:		P
+ *   create a list:		C
+ *   insert an item:		I<nnn>
+ *   remove an item:		R
+ *   print a list:		P
  *
  * Do reasonable checking for dealing with invalid input.  See below.
  *
  * Example:
  *
- *  $ ./a.out C I1 I2 I3 I4 I5 P R P I6 P I7 P R P R P R P R R R P I88 P R P
- *  List: 1 2 3 4 5
- *  List: 2 3 4 5
- *  List: 2 3 4 5 6
- *  List: 2 3 4 5 6 7
- *  List: 3 4 5 6 7
- *  List: 4 5 6 7
- *  List: 5 6 7
- *  List: EMPTY
- *  List: 88
- *  List: EMPTY
+ *   $ ./a.out C I1 I2 I3 I4 I5 P R P I6 P I7 P R P R P R P R R R P I88 P R P
+ *   List: 1 2 3 4 5
+ *   List: 2 3 4 5
+ *   List: 2 3 4 5 6
+ *   List: 2 3 4 5 6 7
+ *   List: 3 4 5 6 7
+ *   List: 4 5 6 7
+ *   List: 5 6 7
+ *   List: EMPTY
+ *   List: 88
+ *   List: EMPTY
  *
- * $ ./a.out C R
- * a.out: Cannot remove an item from an empty list.
+ *   $ ./a.out C R
+ *   a.out: Cannot remove an item from an empty list.
  *
- * $ ./a.out C P
- * List: EMPTY
+ *   $ ./a.out C P
+ *   List: EMPTY
  *
- * $ ./a.out C L
- * a.out: Wrong command: L
+ *   $ ./a.out C L
+ *   a.out: Wrong command: L
  *
- * $ ./a.out C C
- * a.out: List already created, exiting.
+ *   $ ./a.out C C
+ *   a.out: List already created, exiting.
  *
- * $ ./a.out I88
- * a.out: List not created yet.
+ *   $ ./a.out I88
+ *   a.out: List not created yet.
  */
 
 #include <sys/types.h>
