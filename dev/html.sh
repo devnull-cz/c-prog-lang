76ffb471bf0fc1989fc0123e0d5609b587e73739
Vladimir Kotal <vlada@devnull.cz>
2019-09-17 17:55:29 +0200

    push

--- dev/html.sh
+++ dev/html.sh
@@ -19,6 +19,12 @@ git config --global user.email "vlada@devnull.cz"
 git config --global user.name "vladak"
 
 lecture_dir=lecture-notes
-cd repo/$lecture_dir/2020
+cd repo
+repodir=$PWD
+cd $lecture_dir/2020
 
 grip --pass "$GH_PAGES_TOKEN" --export *.md
+mv *.html $repodir/docs
+cd $repodir/docs
+git add -f *.html
+git push -fq origin master
