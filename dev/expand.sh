3358a514f933872e760410a4de6662abbc38e483
Vladimir Kotal <vlada@devnull.cz>
2020-02-25 20:38:59 +0100

    generate all

---
+++ dev/expand.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Expand markdown files (particularly includes)
+
+if [[ -x ./node_modules/markdown-include/bin/cli.js ]]; then
+	cli=./node_modules/markdown-include/bin/cli.js
+elif [[ -x ~/node_modules/markdown-include/bin/cli.js ]]; then
+	cli=~/node_modules/markdown-include/bin/cli.js
+else
+	echo "Unknown location of cli.js for markdown-include"
+	exit 1
+fi
+
+for jsonfile in input/2020/*.json; do
+	$cli "$jsonfile"
+done
