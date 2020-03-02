435837149de52c8e9ca6a2e0259f83fa77554e53
Jan Pechanec <jp@devnull.cz>
2020-03-02 17:58:37 +0100

    Simplified.

--- src/ascii-hex.c
+++ src/ascii-hex.c
@@ -5,15 +5,20 @@ int
 main(void)
 {
 	unsigned char c = 0;
+
 	while (c < 128) {
 		if (isprint(c))
-			printf("%2x %c   ", c, c);
+			printf("%02x %c\t", c, c);
 		if (!isprint(c))
-			printf("%2x NP  ", c);
+			printf("%02x NP\t", c);
 
-		if (c != 0 && (c + 1) % 8 == 0)
+		if ((c + 1) % 8 == 0)
 			printf("\n");
 
+		/*
+		 * NEVER do "if ((c++ + 1) % 8 == 0)" above as that is hard to
+		 * read and such code is prone to errors.
+		 */
 		c++;
 	}
 }
