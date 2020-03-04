5d1cdfdbb2a9d8501e375700d8de96fa80850347
Jan Pechanec <jp@devnull.cz>
2020-03-04 09:22:54 +0100

    On strings.

--- src/array-char-nozero.c
+++ src/array-char-nozero.c
@@ -4,8 +4,15 @@ int
 main(void)
 {
 	char s[] = { 'f', 'o', 'o' };
+	/*
+	 * Note the continuation line.  Indent such a line by 4 characters
+	 * instead of a tabulator.  Always stick to 80 columns.
+	 */
+	char s2[] = { 'f', 'o', 'o', 'f', 'o', 'o', 'f', 'o', 'o', 'f', 'o',
+	    'o', 'f', 'o', 'o', 'f', 'o', 'o' };
 
 	printf("%s\n", s);
+	printf("%s\n", s2);
 
 	return (0);
 }
