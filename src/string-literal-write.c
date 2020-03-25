e487736b1550c3c4a4ad1ee7ec40859043765104
Vladimir Kotal <vlada@devnull.cz>
2020-03-25 10:18:36 +0100

    add array

--- src/string-literal-write.c
+++ src/string-literal-write.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 char *s = "foo";
+char a[] = "bar";
 
 int
 main(void)
 {
-	// causes segmentation failure (with GCC)
-	s[0] = 'A';
+	a[0] = 'A';
+	printf("%s\n", a);
 
+	// causes segmentation failure (with GCC)
+	s[0] = 'B';
 	printf("%s\n", s);
 
 	return (0);
