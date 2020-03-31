b4212e027ebee387ea8fe93bf0c84d9f9d798455
Vladimir Kotal <vlada@devnull.cz>
2020-03-31 17:43:42 +0200

    chdir to repo first

--- dev/expand.sh
+++ dev/expand.sh
@@ -4,6 +4,7 @@ YEAR=2020
 
 # Expand markdown files (particularly includes)
 
+cd repo
 if [[ -x ./node_modules/markdown-include/bin/cli.js ]]; then
 	cli=./node_modules/markdown-include/bin/cli.js
 elif [[ -x ~/node_modules/markdown-include/bin/cli.js ]]; then
