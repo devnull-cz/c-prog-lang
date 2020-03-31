67f8c6128707eff478b7b66cc488fb92cff21f0c
Vladimir Kotal <vlada@devnull.cz>
2020-03-31 17:46:31 +0200

    fix cli path

--- dev/expand.sh
+++ dev/expand.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+#
+# Expand markdown files (particularly includes)
+#
 
 YEAR=2020
 
-# Expand markdown files (particularly includes)
-
 if [[ -x ./node_modules/markdown-include/bin/cli.js ]]; then
-	cli=./node_modules/markdown-include/bin/cli.js
+	cli=$PWD/node_modules/markdown-include/bin/cli.js
 elif [[ -x ~/node_modules/markdown-include/bin/cli.js ]]; then
 	cli=~/node_modules/markdown-include/bin/cli.js
 else
