2ac326f79ebabdfb5e43ecf017df18623e3856f0
Vladimir Kotal <vlada@devnull.cz>
2020-04-01 00:24:47 +0200

    suppress some errors

--- dev/compile_src.sh
+++ dev/compile_src.sh
@@ -3,4 +3,6 @@
 set -e
 
 ls -1 src/*.c | grep -vFf src/blacklist.txt | xargs -n 1 \
-    cc -c -Werror -Wall -Wextra -std=c99
+    cc -c -Werror -Wno-error=unused-but-set-variable \
+    -Wno-error=sign-compare -Wno-error=unused-but-set-variable \
+    -Wall -Wextra -std=c99
