ae5fb66c63443ce6832423eb2f65c9b168938e1b
Vladimir Kotal <vlada@devnull.cz>
2020-03-31 21:46:52 +0200

    fix order

--- dev/expand.sh
+++ dev/expand.sh
@@ -5,11 +5,6 @@
 
 YEAR=2020
 
-lecture_dirname=lecture-notes # where the Markdown files are generated
-if [[ ! -d $lecture_dirname ]]; then
-	mkdir "$lecture_dirname"
-fi
-
 if [[ -x ./node_modules/markdown-include/bin/cli.js ]]; then
 	cli=$PWD/node_modules/markdown-include/bin/cli.js
 elif [[ -x ~/node_modules/markdown-include/bin/cli.js ]]; then
@@ -20,6 +15,12 @@ else
 fi
 
 cd master
+
+lecture_dirname=lecture-notes # where the Markdown files are generated
+if [[ ! -d $lecture_dirname ]]; then
+	mkdir "$lecture_dirname"
+fi
+
 for jsonfile in input/$YEAR/*.json; do
 	$cli "$jsonfile"
 done
