81c0c1b090fe7f47dfcb9076a067766e986d4e81
Vladimir Kotal <vlada@devnull.cz>
2020-04-01 13:34:13 +0200

    build links

--- dev/expand.js
+++ dev/expand.js
@@ -13,15 +13,16 @@ var markdownJson = myArgs[0]
 console.info("processing " + markdownJson);
 var markdownInclude = require('markdown-include');
 
-var BASE_SRC_LINK = "https://github.com/devnull-cz/c-prog-lang/blob/master/src/";
-var BASE_MODULE_LINK = "https://github.com/devnull-cz/c-prog-lang/blob/master/modules/";
+var BASE_LINK = "https://github.com/devnull-cz/c-prog-lang/blob/master/"
+var SRC_LINK = BASE_LINK + "src/";
+var MODULE_LINK = BASE_LINK + "modules/";
 
 markdownInclude.registerPlugin({
 	pattern: /#source .+/gm,
 	replace: function (tag) {
 		var res = tag.split("#source ");
 		var file = res[1];
-		return res[0] + ':eyes: [' + file + "](" + BASE_SRC_LINK + file + ")";
+		return res[0] + ':eyes: [' + file + "](" + SRC_LINK + file + ")";
 	}
 });
 
@@ -32,7 +33,7 @@ markdownInclude.registerPlugin({
 		var idx = val.indexOf(" ");
 		var file = val.substring(0, idx);
 		var linkname = val.substring(idx + 1);
-		return '[' + linkname + "](" + BASE_MODULE_LINK + file + ")";
+		return '[' + linkname + "](" + MODULE_LINK + file + ")";
 	}
 });
 
