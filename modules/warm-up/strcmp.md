88bd9273fa54f0cd1d4ba1a562a237fd3d2ed25f
Vladimir Kotal <vlada@devnull.cz>
2020-04-07 11:44:28 +0200

    add strcmp() warm-up

---
+++ modules/warm-up/strcmp.md
@@ -0,0 +1,11 @@
+:task: # warm-up: implement strcmp()
+
+Implement
+
+    int strcmp(const char *s1, const char *s2);
+
+returns
+> an integer greater than, equal to, or less than 0, according as the
+> string s1 is greater than, equal to, or less than the string s2.
+
+#source strcmp.c
---
+++ src/strcmp.c
@@ -0,0 +1,43 @@
+#ifdef TEST
+#include <stdio.h>
+#include <string.h>	// for testing
+#else
+#define	mystrcmp strcmp
+#endif
+
+#include <assert.h>
+
+static int
+mystrcmp(const char *s1, const char *s2)
+{
+	int ret = 0;
+
+	while (*s1 != '\0' && *s2 != '\0') {
+		if (*s1 > *s2)
+			return 1;
+		else if (*s1 < *s2)
+			return -1;
+
+		s1++;
+		s2++;
+	}
+
+	if (*s1 != '\0')
+		return 1;
+	else if (*s2 != '\0')
+		return -1;
+
+	return ret;
+}
+
+int
+main(void)
+{
+	assert(strcmp("abc", "abd") < 0);
+	assert(strcmp("foo", "bar") > 0);
+	assert(strcmp("foo", "foo") == 0);
+	assert(strcmp("foo", "fooz") < 0);
+	assert(strcmp("fooz", "foo") > 0);
+
+	return (0);
+}
