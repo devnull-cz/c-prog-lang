ee182c43d99139b534e713307f631abb40a7de07
Jan Pechanec <jp@devnull.cz>
2020-04-14 23:53:34 +0200

    Removing stuff from old 08.md.

--- src/ptr-ptr-array.c
+++ src/ptr-ptr-array.c
@@ -3,37 +3,35 @@
 void
 first(int **p)
 {
-	printf("%p = ", *p);
-	fflush(stdout);
-	printf("%d\n", **p);
+	printf("%p = %d\n", *p, **p);
 }
 
 void
 second(int **p)
 {
-	printf("%p = ", *(p + 1));
-	fflush(stdout);
-	printf("%d\n", *(*p + 1));
-	// printf("%d\n", *(*p + 1));
+	printf("%p = %d (0x%x)\n", *(p + 1), **(p + 1), **(p + 1));
 }
 
 int
 main(void)
 {
-	int i = 42, k = 3, j = 7;
-	int *a[] = { &i, &j };
-	int b[] = { i, j };
-
-	printf("&i = %p\n", &i);
-	printf("&j = %p\n", &j);
-	printf(" b = %p\n", b);
-
+	int i = 42, j = 7, k = 99;
+	int *a[] = { &i, &j, &k };
 	int **ppa = a;
+
+	printf(" &i = %p (%d)\n", &i, i);
+	printf(" &j = %p (%d)\n", &j, j);
+	printf(" &k = %p (%d)\n", &k, k);
+	printf("  a = %p\n", a);
+
+
+	int b[] = { i, j, k };
 	int *pb = b;
-
-	printf("pb = %p [%d]\n", pb, *pb);
-
 	int **ppb = &pb;
+
+	printf("  b = %p\n", b);
+	printf(" pb = %p [*pb = %d]\n", pb, *pb);
+	printf("ppb = %p [*ppb = %p]\n", ppb, *ppb);
 
 	printf("first:\n");
 	first(ppa);
@@ -42,6 +40,4 @@ main(void)
 	printf("second:\n");
 	second(ppa);
 	second(ppb);
-
-	return (0);
 }
