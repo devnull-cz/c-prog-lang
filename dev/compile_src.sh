d066a6fd159c4df8261452fc7fc8f5a98e263f8a
Vladimir Kotal <vlada@devnull.cz>
2020-05-03 22:59:46 +0200

    compile source using Makefile
    fixes #9

--- dev/compile_src.sh
+++ dev/compile_src.sh
@@ -2,7 +2,5 @@
 
 set -e
 
-ls -1 src/*.c | grep -vFf src/blacklist.txt | xargs -n 1 \
-    cc -c -Werror -Wno-error=unused-but-set-variable \
-    -Wno-error=sign-compare -Wno-error=unused-but-set-variable \
-    -Wall -Wextra -std=c99
+cd src
+make
