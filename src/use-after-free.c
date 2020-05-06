7aaeca6228d2655ea4d6cd1af2d92fc0ba0cd0ad
Vladimir Kotal <vlada@devnull.cz>
2020-05-06 10:07:58 +0200

    add missing file

---
+++ src/use-after-free.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+static char *
+foo(size_t len)
+{
+	return ((char *)malloc(len));
+}
+
+int
+main(void)
+{
+	char *p = (char *)malloc(128);
+
+	*p = 'A';
+	free(p);
+	*p = 'B';
+
+	char *q = foo(16);
+	*q = 'X';
+	free(q);
+	*q = 'Y';
+
+	return (0);
+}
