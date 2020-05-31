cdb23c755540cfad0a1377b89473d88fd2b7fdf3
Jan Pechanec <jp@devnull.cz>
2020-05-31 17:55:11 +0200

    More work on source code.

--- src/foo-consumer.c
+++ src/foo-consumer.c
@@ -5,11 +5,11 @@
 int
 main(void)
 {
-	struct foo *foo; // has to be a pointer
+	foo_t h; // has to be a pointer
 
 	// will not compile:
 	// printf("%zu\n", sizeof (struct foo));
 
-	foo = getFoo();
-	doStuff(foo);
+	h = getFoo();
+	doStuff(h);
 }
--- src/foo.h
+++ src/foo.h
@@ -1,4 +1,6 @@
 struct foo;
 
+typedef struct foo *foo_t;
+
 void doStuff(struct foo *f);
 struct foo *getFoo(void);
