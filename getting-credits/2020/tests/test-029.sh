548baa80ab89a37cca8f317de455c3fd522d83fa
Jan Pechanec <jp@devnull.cz>
2020-07-25 14:30:49 +0200

    Extended test to check the file contents as well.

--- getting-credits/2020/tests/test-029.sh
+++ getting-credits/2020/tests/test-029.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 #
 # The assignment says files up to 2 GiB are supported. Test archive
-# creation/extraction with a 1 GiB file. This is simple smoke test.
+# creation/extraction with a 1 GiB file and check the file contents as well.
 #
 
 source $configvar
@@ -9,7 +9,7 @@ cd $tmpdir
 tmp_path=$PWD
 
 tmp2=tmp-$(basename $0)
-mkdir $tmp2
+mkdir $tmp2 || { echo "mkdir failed" && exit 1; }
 cd $tmp2
 tmp2_path=$PWD
 
@@ -20,28 +20,31 @@ dd if=/dev/urandom of=$bigfile bs=1024 count=$count >/dev/null 2>&1
 ((ret == 0)) || { echo "Cannot create big file." >&2 && exit 1; }
 
 # create
-outfile=$tmp_path/big.tar
+outfile=$tmp2_path/big.tar
 output=$($GNUTAR -c -v -f $outfile * 2>&1)
 typeset -i ret=$?
 ((ret == 0)) || { echo "Wrong GNU tar return value $ret." >&2 && exit 1; }
 echo "$output" | sed -e "s/^[a-zA-Z0-9/.]*$base: /mytar: /"
-cd ..
 
-# cleanup
+hash1=$(openssl md5 $bigfile | cut -f2 -d=)
+
+# remove the file we used to create the archive
 typeset -i exp_size=$(ls -sk $bigfile | cut -f1 -d " ")
-rm "$bigfile"
+rm $bigfile
 ((ret == 0)) || { echo "Cannot remove big file." >&2 && exit 1; }
-rmdir "$tmp2_path"
 
 # extract
-output=$($MYTAR -x -v -f "$outfile" 2>&1)
+output=$($MYTAR -x -v -f $outfile 2>&1)
 typeset -i ret=$?
 echo "$output" | sed -e "s/^[a-zA-Z0-9/.]*$base: /mytar: /"
 ((ret == 0)) || { echo "Wrong tar return value $ret." >&2 && exit 1; }
 
 # check size of the extracted file
 typeset -i act_size=$(ls -sk $big_name | cut -f1 -d " ")
-((exp_size != act_size)) && \
+((exp_size == act_size)) || \
     { echo "File sizes differ: $exp_size $act_size" >&2 && exit 1; }
 
-exit 0
+hash2=$(openssl md5 $big_name | cut -f2 -d=)
+[[ $hash1 == $hash2 ]] || \
+    { printf "Extracted file '$(pwd)/$big_name' differs from the source.\n" \
+        "hash1:$hash\nhash2:$hash2\n" >&2 && exit 1; }
