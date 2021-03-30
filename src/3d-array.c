2dc0b4a466ed568c94b571405c42f3a0853035f7
Jan Pechanec <jp@devnull.cz>
2021-03-30 23:15:52 +0200

    Fixed cstyle.

--- src/3d-array.c
+++ src/3d-array.c
@@ -1,11 +1,10 @@
 /*
-  compile and then run in gdb like this:
-
-    break foo
-    r
-    step
-    p/x (int [12])*<insert the address printed here>
-
+ * Compile and then run in gdb like this:
+ *
+ *   break foo
+ *   r
+ *   step
+ *   p/x (int [12])*<insert the address printed here>
  */
 
 #include <stdio.h>
@@ -19,9 +18,9 @@ int
 main(void)
 {
 	int a[][2][3] = {
-                          { {1,2,3}, {4,5,6} },
-                          { {8,7,8}, {9,1,9} },
-                        };
+	    { {1,2,3}, {4,5,6} },
+	    { {8,7,8}, {9,1,9} },
+	};
 
 	printf("%p\n", a);
 	foo();
