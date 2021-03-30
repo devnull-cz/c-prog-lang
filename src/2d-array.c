5888fb723d4434ae7da2fcea2c9cdbdd127f9a35
Jan Pechanec <jp@devnull.cz>
2021-03-30 23:37:47 +0200

    Added a comment.

--- src/2d-array.c
+++ src/2d-array.c
@@ -3,13 +3,22 @@
  */
 #include <stdio.h>
 
+/*
+ * Remember, an array in C is just a piece of consequtive memory, and it does
+ * not carry around any information about its size.  So, we must either know how
+ * many lines (= the first dimension) is there, or provide it as an argument.
+ *
+ * Any other dimensions short of the 1st one must be exactly specified otherwise
+ * the compiler will complain about an incomplete type (= type whose size cannot
+ * be determined).
+ */
 void
-func(int array[][2], size_t size)
+func(int array[][2], size_t nlines)
 {
 	printf("sizeof = %zu\n", sizeof (array[0]));
 
 	int i = 0;
-	while (i < size) {
+	while (i < nlines) {
 		printf("%d\n", array[i][0]);
 		i++;
 	}
