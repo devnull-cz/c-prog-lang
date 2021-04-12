478d3aa54c3f72e402bb62dd9914898a999a53f9
Jan <jp@devnull.cz>
2021-04-12 23:27:21 +0200

    Add more comments.

--- src/compound-statement.c
+++ src/compound-statement.c
@@ -3,11 +3,24 @@
 int
 main(void)
 {
-	{ // start of block (compound statement)
-		int i = 8;	// variable declaration with initialization
-		printf("%d\n", i);	// expression statement
-	} // end of block
-	int i = 12;	// reuse
+	int i = 0;
 
+	{ /* Start of block (compound statement) */
+		/* variable declaration with initialization */
+		int i = 8;
+
+		/* 
+		 * expression statement (note that a function call is just an
+		 * expression in C).
+		 */
+		printf("%d\n", i);
+	} /* End of block statement, no semicolon needed (but allowed) */
+
+	/*
+	 * This 'i' is not related to the 'i' variable used in the block above.
+	 */
+	++i;
+
+	/* Will return 1 */
 	return (i);
 }
