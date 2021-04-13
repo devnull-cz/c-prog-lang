1a1c2895f787993566a04c42a7049124dd3777b8
Jan <jp@devnull.cz>
2021-04-13 22:45:44 +0200

    Improved comments.

--- src/implement-function.c
+++ src/implement-function.c
@@ -20,19 +20,20 @@ main(void)
         char a[1024];
         char *s = "hello, world";
         char *s2 = \
-	    "I watched C-beams glitter in the dark near the Tannhauser Gate";
+	    "I watched C-beams glitter in the dark near the Tannhauser Gate.";
 
         assert(strlen (s) < sizeof (a));
         assert(strlen (s2) < sizeof (a));
 
+	/* Must print 13 */
         printf("Chars copied: %zu\n", copy_s2a(a, s));
         /* This must print "hello, world" */
         printf("%s\n", a);
 
-	/* Must print 62. */
+	/* Must print 63. */
         printf("s2 len: %zu\n", strlen(s2));
-	/* Must print 63 as the terminating '\0' was also copied. */
+	/* Must print 64 as the terminating '\0' was also copied. */
         printf("Chars copied: %zu\n", copy_s2a(a, s2));
-        /* This must print the quote. */
+        /* This must print the full quote. */
         printf("%s\n", a);
 }
