25717849a8b216ace0e430820e286f4da284902b
Vladimir Kotal <vlada@devnull.cz>
2021-04-20 21:26:01 +0200

    add error to catch multiple includes

--- src/animals.h
+++ src/animals.h
@@ -1,3 +1,6 @@
+#ifndef _ANIMALS_H_
+#define _ANIMALS_H
+
 #include "animal.h"
 
 struct animal animals[] = {
@@ -10,3 +13,7 @@ struct animal animals[] = {
 	{ "ant", 6 },
 	{ "centipede", 100 }
 };
+#else
+/* because this file contains definition of array we want to be extra careful */
+#error "multiple includes of animal definitions !"
+#endif /* _ANIMALS_H_ */
