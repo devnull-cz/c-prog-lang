71e585a6a1826b3ae243f03134359d3f60deddfc
Jan <jp@devnull.cz>
2021-04-27 21:05:18 +0200

    cstyled.

--- src/strcmp.c
+++ src/strcmp.c
@@ -19,20 +19,20 @@ mystrcmp(const char *s1, const char *s2)
 
 	while (*s1 != '\0' && *s2 != '\0') {
 		if (*s1 > *s2)
-			return 1;
+			return (1);
 		else if (*s1 < *s2)
-			return -1;
+			return (-1);
 
 		s1++;
 		s2++;
 	}
 
 	if (*s1 != '\0')
-		return 1;
-	else if (*s2 != '\0')
-		return -1;
+		return (1);
+	if (*s2 != '\0')
+		return (-1);
 
-	return ret;
+	return (ret);
 }
 
 int
