c2507b816b9f273becf2a02ada1adbf1a70efb51
Vladimir Kotal <vlada@devnull.cz>
2021-05-04 21:51:45 +0200

    add comment + print the original string

--- src/strsep.c
+++ src/strsep.c
@@ -5,11 +5,17 @@ int
 main(void)
 {
 	char *p;
-	char str[] = "foo,,bar";
+	char str[] = "foo,,bar";  // There is good reason this is array.
+	char *end = str + strlen(str) + 1;
 	char *inputstr = str;
 	char *delim = ",";
 
 	while ((p = strsep(&inputstr, delim)) != NULL) {
 		printf("'%s'\n", p);
 	}
+
+	// Print the original string to see how it changed.
+	for (p = str; p < end; p++)
+		printf("%x ", *p);
+	printf("\n");
 }
