587cfcc302341312a3e93b8df276932c078e8ee0
Vladimir Kotal <vlada@devnull.cz>
2021-05-05 10:40:58 +0200

    add print before and after

--- src/strsep.c
+++ src/strsep.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+static void
+print_hex(char *ident, char *str, char *end)
+{
+	char *p;
+
+	for (p = str; p < end; p++)
+		printf("%2hhx ", *p);
+
+	printf("\n");
+}
+
 int
 main(void)
 {
@@ -10,12 +21,13 @@ main(void)
 	char *inputstr = str;
 	char *delim = ",";
 
+	// Print the original string.
+	print_hex("before: ", str, end);
+
 	while ((p = strsep(&inputstr, delim)) != NULL) {
 		printf("'%s'\n", p);
 	}
 
 	// Print the original string to see how it changed.
-	for (p = str; p < end; p++)
-		printf("%hhx ", *p);
-	printf("\n");
+	print_hex("after: ", str, end);
 }
