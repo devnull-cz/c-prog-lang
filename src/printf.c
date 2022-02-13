2e39d63a30650eb664dd903690812d9792fe3f2f
Jan <jp@devnull.cz>
2022-02-13 20:39:08 +0100

    Fixed typo.

--- src/printf.c
+++ src/printf.c
@@ -7,7 +7,7 @@ main(void)
 	printf("hello, world\n");
 	/* Strings are concatenated automatically (in C99, not in C89). */
 	printf("hello, world\n"
-	    "hello world\n");
+	    "hello, world\n");
 #if 0
 	/*
 	 * This will return an error as a new line must not be part of a string
