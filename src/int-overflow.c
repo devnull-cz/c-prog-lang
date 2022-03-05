73bf89f3bc7cab9b1d965c682e69cb73707c1e17
Jan <jp@devnull.cz>
2022-03-05 23:18:30 +0100

    On integer overflow.

--- src/int-overflow.c
+++ src/int-overflow.c
@@ -1,12 +1,19 @@
 /*
- *  compare the assembly of foo() when compiled with -O0 and -O3
+ *  Compare the assembly of foo() when compiled with -O0 and -O3
  *
  *  E.g. in GCC 5.4.x you will see that bar() has been optimized away.
  *  This is because signed integer overflow is undefined by the standard
  *  so assuming that it will cause wraparound is not valid.
  *
- *  try to compile with -fstrict-overflow -Wstrict-overflow -O2
- *  (should produce a warning)
+ *  Try to compile with -fstrict-overflow -Wstrict-overflow -O2
+ *  (should produce a warning XXX it does not with gcc 11.2.0 on 64-bit)
+ *
+ *	$ gcc int-overflow.c 
+ *	$ ./a.out 500
+ *	bar -2147483452
+ *	$ gcc -O3 int-overflow.c 
+ *	$ ./a.out 500
+ *	$ 
  */
 
 #include <stdio.h>
