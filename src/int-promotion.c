1935f78e7e7712128fe1f3f22fcedbc3fb91c558
Jan <jp@devnull.cz>
2022-03-05 22:47:15 +0100

    Fixed comment.

--- src/int-promotion.c
+++ src/int-promotion.c
@@ -4,16 +4,16 @@ int
 main(void)
 {
 	unsigned char a = 0xff;
+	/* This will probably wrap around but it is undefined by the spec. */
 	signed char b = 0xff;
 
-	int c = a == b; // true, or false ?
+	int c = (a == b); // true, or false ?
 
 	/*
 	 * Note that 'a' and 'b' are first promoted to ints (integer promotion)
 	 * when they are being compared.
 	 *
-	 * Also, %x converts the arguments to UNSIGNED ints before printing
-	 * those.
+	 * Also, %x takes the arguments as UNSIGNED ints.
 	 */
 	printf("A: %08x, B: %08x\n", a, b);
 	printf("C: %d\n", c);
