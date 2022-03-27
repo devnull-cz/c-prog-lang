171205278122756e14d12b0708a7744bc2a115e9
Jan <jp@devnull.cz>
2022-03-27 20:49:10 +0200

    Added one more example.

--- src/string-literal-address.c
+++ src/string-literal-address.c
@@ -7,4 +7,10 @@ main(void)
 
 	printf("%p\n", s = "hello world");
 	printf("%s\n", s);
+	/*
+	 * This probably prints the same address as the first printf() as only
+	 * one copy of the string is usually stored (usually in a read-only
+	 * segment).
+	 */
+	printf("%p\n", "hello world");
 }
