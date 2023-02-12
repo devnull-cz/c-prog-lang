1305d1f54260bfcca7f10498cab2065e273bebc1
Jan <jp@devnull.cz>
2023-02-12 15:48:54 +0100

    Added a comment.

--- src/float.c
+++ src/float.c
@@ -3,11 +3,10 @@
 int
 main(void)
 {
-	float f;
-
-	f = 13.12345678;
+	float f = 13.12345678;
 
 	printf("%f\n", f);
+	/* Will this print exactly 13.12345678?  If not, why? */
 	printf("%.8f\n", f);
 	printf("%.2f\n", f);
 	printf("%10.2f\n", f);
