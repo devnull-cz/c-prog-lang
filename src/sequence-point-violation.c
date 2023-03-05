07e648518344c801a88487de6d88a32e7894aeaa
Jan <jp@devnull.cz>
2023-03-05 22:10:16 +0100

    More info on sequence point violations.

--- src/sequence-point-violation.c
+++ src/sequence-point-violation.c
@@ -1,9 +1,26 @@
+/*
+ * See C99, 6.5 Expressions, para 2, for more information.
+ */
 int
 main(void)
 {
-	int a;		// not initialized
+	int a = 0;
+	int b[10];
 
-	a = a++;	// sequence point violation
+	/*
+	 * Sequence point violation as an object may have its value modified at
+	 * most once between sequence points.  Here we try to modify it twice.
+	 */
+	a = a++;
 
-	return (a);
+	/*
+	 * Another sequence point violation.  This one relates to the second
+	 * sentence in paragraph 2: "Furthermore, the prior value shall be read
+	 * only to determine the value to be stored".  That means that if the
+	 * object is read (used), it must be used to compute the resulting
+	 * value.  The use of 'a' as an index violates that.
+	 */
+	b[a] = a++;
+
+	return (b[a]);
 }
