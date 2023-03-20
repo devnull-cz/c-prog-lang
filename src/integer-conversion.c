7500cc32aa0b91a33345e8e4e6a1bc92cdf8c7ff
Vladimir Kotal <vlada@kotalovi.cz>
2023-03-20 08:45:29 +0100

    fix variable name in comment

--- src/integer-conversion.c
+++ src/integer-conversion.c
@@ -27,14 +27,14 @@ main(void)
 	printf("%hhu\n", c);
 
 	/*
-	 * Assuming 'i' is 4 byte in size, the smallest integer congruent with
+	 * Assuming 'u' is 4 byte in size, the smallest integer congruent with
 	 * -10 modulo 2^32 that fits an int is (-10 + 2^32).  Use bc(1):
 	 *
 	 *	$ bc
 	 *	-10+2^32
 	 *	4294967286
 	 *
-	 * So it means 'i' will be assigned 4294967286.
+	 * So it means 'u' will be assigned 4294967286.
 	 */
 	u = -10;
 	printf("%u\n", u);
