429bd853f60fe9a821fdb4a409939dfe7991050a
Jan <jp@devnull.cz>
2023-04-02 18:59:19 +0200

    Simplified.

--- src/strcmp.c
+++ src/strcmp.c
@@ -20,7 +20,7 @@ mystrcmp(const char *s1, const char *s2)
 	while (*s1 != '\0' && *s2 != '\0') {
 		if (*s1 > *s2)
 			return (1);
-		else if (*s1 < *s2)
+		if (*s1 < *s2)
 			return (-1);
 
 		s1++;
