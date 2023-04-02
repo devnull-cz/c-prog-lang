4bb22f429fb2f450814a5e62136041782e9dd169
Jan <jp@devnull.cz>
2023-04-02 18:47:30 +0200

    Quell a few unneeded warnings.

--- src/passing-arrays.c
+++ src/passing-arrays.c
@@ -1,6 +1,11 @@
+#include <stdio.h>
+
 void
 myfn(int a[2][2])
 {
+	/* Silence the unused parameter warning. */
+	(void)a;
+
         return;
 }
 
@@ -11,8 +16,8 @@ main(void)
 	 * You need () as "*p[2]" would be "p is an array of pointers to int" as
 	 * [] has higher priority.  More on that later.
 	 */
-        int (*p)[2];
-        int (*p2)[3];
+        int (*p)[2] = NULL;
+        int (*p2)[3] = NULL;
 	/*
 	 * As the the array expression is converted to a pointer, the first
 	 * dimension is irrelevant.  All other dimensions are not.
