f0c50bd2ead622f988d102eb32a672e0d50bb2bc
Jan Pechanec <jp@devnull.cz>
2025-01-27 21:52:54 +0100

    Better comment.

--- src/2d-static-array-as-1d.c
+++ src/2d-static-array-as-1d.c
@@ -19,15 +19,19 @@ main(void)
 
 	/*
 	 * Print the consequtive memory as a one-dimensional array. Note that
-	 * "a" is just a pointer so you can re-type it to a different pointer
-	 * type.
+	 * "a" is just a pointer so you can re-type it to a pointer type of its
+	 * aggregate (or subaggregate) member.  That means strict aliasing rules
+	 * are followed.
 	 *
 	 * We could also do:
 	 *
 	 *	p_i = &a[0][0];
 	 *	p_i = a[0];	// no need for a cast either
+	 *
+	 * You need the cast in the following statement to silence a warning
+	 * ('a' here is a pointer to an array).
 	 */
-	p_i = (int *)a;		// you need the cast to silence a warning
+	p_i = (int *)a;
 	for (i = 0; i < MYSIZE * MYSIZE; ++i) {
 		printf("%d\n", p_i[i]); // could also do ((int *)a)[i] directly
 #if 0
