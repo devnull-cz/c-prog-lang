91e29e6ad320b7d2c3b386b87483e0904f6edbac
Vladimir Kotal <vlada@kotalovi.cz>
2025-04-01 22:15:34 +0200

    resolve unused modules

--- dev/check-unused-modules.sh
+++ dev/check-unused-modules.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-ret=$( find modules/ -type f -name '*.md' | while read f; do
-	if ! grep "$f\"" input/*/*.md >/dev/null; then
-		echo $f;
+# First, find modules not used directly in the input files.
+not_in_input=$( find modules/ -type f -name '*.md' | while read f; do
+	f=$(basename $f)
+	if ! grep "^#include \"modules\/.*$f\"" input/*/*.md >/dev/null; then
+		echo "$f";
 	fi;
 done; )
+
+# Next, see if these are referenced from any other module.
+ret=$( for f in $not_in_input; do
+	if ! grep "#module $f" modules/*.md >/dev/null; then
+		echo "$f"
+	fi
+done )
 
 if [[ -n $ret ]]; then
 	echo "$ret"
--- modules/object.md
+++
@@ -1,6 +0,0 @@
-## Object
-
-	C99 3.14
-
-	*object* is a region of data storage in the execution environment, the
-	contents of which can represent values
