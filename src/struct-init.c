60cc248925d473bb484cb0781c5ecd5ebb259cfa
Vladimir Kotal <vlada@kotalovi.cz>
2025-05-05 21:48:49 +0200

    fix wording

--- src/struct-init.c
+++ src/struct-init.c
@@ -10,7 +10,7 @@ main(void)
 {
 	struct foo_s foo = { 1, 'C' };
 
-	/* Remember, char is expanded to int if passed to printf */
+	/* Remember, char is promoted to int if passed to printf */
 	printf("%d\n", foo.a);
 	printf("%c\n", foo.b);
 }
