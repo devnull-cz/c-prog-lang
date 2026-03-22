a352ba233dbd31f2e99fc79a7872cba4573eb24b
Vladimir Kotal <vlada@kotalovi.cz>
2026-03-22 11:51:39 +0100

    use macro for computing array length

--- src/solutions/2darray-min-avg.c
+++ src/solutions/2darray-min-avg.c
@@ -2,15 +2,17 @@
 #include <err.h>
 #include <sys/types.h>
 
+#define	ARRAY_LEN(a)	(sizeof (a) / sizeof (a[0]))
+
 int
 main(void)
 {
 	int a[3][2] = { { 1, 44 }, { 1, 0 }, { 2, 33 } };
 	size_t min = -1;
 
-	for (size_t i = 0; i < sizeof (a) / sizeof (a[0]); i++) {
+	for (size_t i = 0; i < ARRAY_LEN(a); i++) {
 		ssize_t sum = 0;
-		for (size_t j = 0; j < sizeof (a[0]) / sizeof (a[0][0]); j++) {
+		for (size_t j = 0; j < ARRAY_LEN(a[0]); j++) {
 			if (a[i][j] <= 0) {
 				sum = -1;
 				break;
@@ -18,7 +20,7 @@ main(void)
 			sum += a[i][j];
 		}
 		if (sum > 0) {
-			size_t avg = sum / (sizeof (a[0]) / sizeof (a[0][0]));
+			size_t avg = sum / ARRAY_LEN(a[0]);
 			if (avg < min)
 				min = avg;
 		}
